e3ed1adfc2c522a743ccd7e1be15af63de80862d
vikram <vikramdev772@gmail.com>
2023-09-28 12:32:05 +0530

    Basic

---
+++ Basics/app.js
@@ -0,0 +1,23 @@
+var a=5;
+var b=6;
+var c=-(-a-b);
+console.log("\n\t sum of two num : "+c);
+
+let x = 10;
+let y = 20;
+let op = "x * y";
+let result = eval(op);
+console.log("\n\t Eval :  "+result);
+
+var n=5;
+let i;
+let ans=1;
+console.log("\n\t Factorial of 5 \n");
+for(i=1;i<=n;i++){
+    console.log(" "+i+" ");
+    ans=ans*i;
+    
+  
+}
+
+console.log("\n\t factorial "+ans);
