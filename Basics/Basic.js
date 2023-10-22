1783b97f7226bba01550efc74d0df512f433cd27
vikram <vikramdev772@gmail.com>
2023-10-22 21:48:14 +0530

    keyword

---
+++ Basics/Basic.js
@@ -0,0 +1,27 @@
+var userName = "Virkam";
+console.log(userName);
+let firstName = "Vik";
+let lname="ram";
+const fn=firstName+lname;
+console.log(fn);
+console.log(firstName);
+firstName=25;
+userName= '123';
+console.log(firstName);
+console.log(userName);
+
+// const a="vikram ram";
+// console.log(a);
+// a=58;
+// console.log(a);
+
+Virkam
+Vikram
+Vik
+25
+123
+vikram ram
+c:\Users\gvikr\Desktop\v k m\ES6\Basics\Basic.js:15
+a=58;
+
+
