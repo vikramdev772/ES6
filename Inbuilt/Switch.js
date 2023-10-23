c7bfad21ed952f1bf1bbafb51de7574a6d06f6f8
vikram <vikramdev772@gmail.com>
2023-10-23 07:38:26 +0530

    switch

---
+++ Inbuilt/Switch.js
@@ -0,0 +1,22 @@
+
+
+let user = "student";
+
+switch (user){
+    case "adim":
+        console.log("\n\t You have got acess  \n");
+        break;
+    case "mentor":
+        console.log("\n\t Mentor got acess  \n");
+        break;
+    case "student":
+        console.log("\n\t Student have got acess  \n");
+        break;
+    default :
+    console.log("\n\t Acess Denined \n");
+}
+
+// Acess Denined 
+
+
+Student have got acess  
