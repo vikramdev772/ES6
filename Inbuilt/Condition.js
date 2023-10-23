0af7ca9d42468354aec90ff26953624a4799bf22
vikram <vikramdev772@gmail.com>
2023-10-23 07:56:37 +0530

    Conditions

---
+++ Inbuilt/Condition.js
@@ -0,0 +1,59 @@
+
+// Mini rating app
+
+let rating = 4;
+let a="4";
+if(rating === 4){ // == 5
+    console.log("\n\t 5  stars \n");
+    console.log("\n\t === matchs the type of value \n");
+}else if(rating == a){
+    console.log("\n\t 4 stars matching the value \n");
+}else{
+    console.log("\n\t  we will improve \n");
+}
+
+// 4 stars matching the value 
+
+//  5  stars 
+
+
+	//  === matchs the type of value 
+
+
+    // Ternary condition
+
+    //Conditon ? exep(true) : exep(false) ;
+
+    login = true ;
+    login ? console.log("\n\t it is true : log out \n") : console.log("\n\t Login karo  \n");
+
+
+    // it is true : log out 
+
+
+    //Interpolation
+
+    let n="Vikram";
+    const id="vk4011";
+    var or="Template literal";
+    let b=" `  ` ";
+    let use = "${ }";
+    console.log(
+        `
+        My name is ${n}
+        My id is ${id}
+        Template String ${or}
+        back ticks ${b}, ${use}
+        `
+    );
+
+
+    
+    // My name is Vikram
+    // My id is vk4011
+    // Template String Template literal
+    // back ticks  `  ` , ${ }
+
+
+    
+
