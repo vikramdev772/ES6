4aba4fd44a7ec5f121953cf9fc2c7dbb6228b274
vikram <vikramdev772@gmail.com>
2023-10-23 08:00:23 +0530

    Operators

---
+++ Inbuilt/Operators.js
@@ -0,0 +1,15 @@
+
+let x=10;
+let y=20;
+let z=x+y;
+console.log(z);  //30
+console.log("\n\t "+"5"+5); //55
+console.log(+"5"+5); //10
+
+console.log(-"2"+5);//3
+console.log(8-9); //-1
+
+const n=25;
+console.log(typeof n); //number
+
+
