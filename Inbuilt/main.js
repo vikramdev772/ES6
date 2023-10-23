8ac5d0413e7fb1979e68628018d45457f1f914f5
vikram <vikramdev772@gmail.com>
2023-10-23 07:45:29 +0530

    main

---
+++ Inbuilt/main.js
@@ -0,0 +1,22 @@
+
+
+// In java script every thing is a object 
+
+//Math Object 
+
+const p=Math.PI;
+console.log(p);  //3.141592653589793
+
+console.log(Math.random()); //0.7288381406041042
+
+console.log(Math.random()*10); //2.919238574078078
+
+console.log(Math.floor(Math.random()*50)); //35 decimal - integers
+
+console.log(Math.floor(Math.random()*10000));// 4832
+
+
+console.log(Math.ceil(Math.random()*10000)); // 9486
+
+
+
