e3bef0c5dfcfc6100e9996a0bcfd0cd045982311
vikram <vikramdev772@gmail.com>
2023-10-26 16:05:29 +0530

    forEach

---
+++ HOF/Each.js
@@ -0,0 +1,13 @@
+//For Each
+
+const arr1=["Harry","potter","Tony","Stark"];
+arr1.forEach((values)=>console.log(values));
+
+// Harry
+// potter
+// Tony
+// Stark
+
+const result = arr1.forEach((v)=>{
+    console.log(v);
+})
