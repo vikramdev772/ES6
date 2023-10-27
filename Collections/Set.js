836f6c36d576e644f4042eb0374fc89f09d577d6
vikram <vikramdev772@gmail.com>
2023-10-27 19:34:51 +0530

    Set

---
+++ Collections/Set.js
@@ -0,0 +1,26 @@
+
+
+let lang = new Set();
+console.log(lang);
+// Set(0) {}
+
+let ln=["html","css","js","html","React","tailwing","React"];
+let lg=new Set(ln);
+console.log(lg);
+
+// Set(5) { 'html', 'css', 'js', 'React', 'tailwing' }
+
+let s=new Set();
+s.add("v");
+s.add("i");
+s.add("k");
+s.add("r");
+s.add("a");
+s.add("m");
+console.log(s);
+// Set(6) { 'v', 'i', 'k', 'r', 'a', 'm' }
+console.log("\n\t Size: "+s.size);
+
+// Size: 6
+
+
