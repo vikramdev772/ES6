51d867ded46dd67888b0f1ae57e8d12d58c0c24f
vikram <vikramdev772@gmail.com>
2023-10-27 19:35:39 +0530

    Map

---
+++ Collections/Map.js
@@ -0,0 +1,13 @@
+let yes = new Map();
+console.log(yes);
+
+yes.set("1","vijay");
+yes.set(1,"vikram");
+yes.set(true,"Mark");
+console.log(yes);
+
+// Map(0) {}
+// Map(3) { '1' => 'vijay', 1 => 'vikram', true => 'Mark' }
+
+
+
